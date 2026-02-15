3ed082d35ac8e9893242694fb93e509e236e0eb3
Tomas Caraccia <tcaraccia@g2.com>
2026-02-15 08:30:43 +0100

    fix: replace placeholder SHA256 hashes with actual v0.1.0 checksums

--- Formula/taskboard.rb
+++ Formula/taskboard.rb
@@ -7,20 +7,20 @@ class Taskboard < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/tcarac/taskboard/releases/download/v#{version}/taskboard-darwin-arm64.tar.gz"
-      sha256 'SHA256_DARWIN_ARM64'
+      sha256 'f79b23fe044ad46b410c5f1bae88862294ca8dd4cceeb9108434f9eabc4acd14'
     elsif Hardware::CPU.intel?
       url "https://github.com/tcarac/taskboard/releases/download/v#{version}/taskboard-darwin-amd64.tar.gz"
-      sha256 'SHA256_DARWIN_AMD64'
+      sha256 'a043cc06d3750a3bd6eeecd95fa5e7e26978fa2d46f396170270851fdde15705'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/tcarac/taskboard/releases/download/v#{version}/taskboard-linux-arm64.tar.gz"
-      sha256 'SHA256_LINUX_ARM64'
+      sha256 'e3904fb7f7e335bce866c1585f2ab8f12270c5e5aed62ec3a058f01bb1b9846f'
     elsif Hardware::CPU.intel?
       url "https://github.com/tcarac/taskboard/releases/download/v#{version}/taskboard-linux-amd64.tar.gz"
-      sha256 'SHA256_LINUX_AMD64'
+      sha256 '60762972c76433ad9aa1843bbb9364004d3fd42f0b6ae9032f08486a1b4c18b8'
     end
   end
 
